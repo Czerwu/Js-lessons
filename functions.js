3feef00b86c7bd148ec3c367ad91c7c8325aca6f
Bartlomiej Sworzen <b.sworzen@gmail.com>
2025-02-24 13:09:14 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,27 +1,24 @@
-// Plik: functions.js
+// String -> "Imie"
+// number -> 5
+// boolean -> true / false
+// undefined -> undefined
+// null -> null
 
-// Funkcja dodawania dwóch liczb
 function dodaj(a, b) {
     return a + b;
 }
 
-// Funkcja odejmowania dwóch liczb
 function odejmij(a, b) {
     return a - b;
 }
 
-// Funkcja mnożenia dwóch liczb
 function pomnoz(a, b) {
     return a * b;
 }
 
-// Funkcja dzielenia dwóch liczb (z zabezpieczeniem przed dzieleniem przez 0)
 function podziel(a, b) {
     if (b === 0) {
         return "Nie można dzielić przez zero!";
     }
     return a / b;
 }
-
-// Eksportowanie funkcji (przydatne, jeśli będziesz używać Node.js)
-module.exports = { dodaj, odejmij, pomnoz, podziel };
