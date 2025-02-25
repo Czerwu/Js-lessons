6969ed764dca91e5f26e5e0f5c88eafae7c65fdb
Bartlomiej Sworzen <b.sworzen@gmail.com>
2025-02-25 14:48:43 +0100

    Update advancedFunctions.js

--- advancedFunctions.js
+++ advancedFunctions.js
@@ -1,3 +1,27 @@
+// const arr = [1, "jablko", true]
+
+//for(let i = 0; i < arr.length; i++) {
+//  console.log(arr[i])
+//}
+
+// krok 1 -> i = 0 ; 0 < 3 ; console.log(1) -> i++
+// krok 2 -> i = 1 ; 1 < 3 ; console.log("jablko") -> i++
+// krok 3 -> i = 2 ; 2 < 3 ; console.log(true) -> i++
+// krok 2 -> i = 3 ; 3 < 3 ; 
+
+// let i = 0
+
+//while(i < arr.length) {
+//  console.log(arr[i])
+//  i++
+//}
+
+// krok 1 -> 0 < 3 ; console.log(1) i++
+// krok 2 -> 1 < 3 ; console.log("jablko") i++
+// krok 3 -> 2 < 3 ; console.log(true) i++
+
+
+
 const tablica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const osoba = {
