94cfb4e9fb0764a5a20edb58dccd6548ce7eaa38
Czerwu <mczerw999@gmail.com>
2025-02-26 10:36:22 +0100

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,35 @@
+
+for (let i = 1; i <= 30; i++) {
+    console.log(i);
+}
+
+
+let i = 1;
+while (i <= 30) {
+    console.log(i);
+    i++;
+}
+
+
+for (let i = 20; i >= 10; i--) {
+    console.log(i);
+}
+
+
+let j = 20;
+while (j >= 10) {
+    console.log(j);
+    j--;
+}
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log('*'.repeat(i));
+}
+
+
+let k = 1;
+while (k <= 5) {
+    console.log('*'.repeat(k));
+    k++;
+}
