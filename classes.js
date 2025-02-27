90f17689dcfbad3b24ea313ad9dedcb49b66b28f
Czerwu <mczerw999@gmail.com>
2025-02-27 12:16:44 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -33,7 +33,7 @@ class Monster {
 }
 
 
-const hero = new Hero("Wojownik", 100, ["Miecz"]);
+const hero = new Hero("Wojownik", 100, ["Miecz", "Maiga"]);
 const monster = new Monster("Smok", 80);
 
 hero.atak(monster);
