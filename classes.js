ae2cc6e9c7d0170c6ae813c1cf1c84dbe13bf01b
Czerwu <mczerw999@gmail.com>
2025-02-27 14:22:44 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,40 +1,32 @@
-//Zamiast tworzyc osobne klasy mozna  stowrzyc jedna bazowoa klase, ktora bedzie miala wspolne wlasciwosci takie jak np. hp, nazwa oraz atak. 
-//Bohater i potor bede otrzymywac te funckje i nie bedizemy duplikowac kodu.
-
-
-
-
-
-
-
-class Hero {
-    constructor(nazwa, hp, skills) {
-        this.nazwa = nazwa;
-        this.hp = hp;
-        this.skills = skills;
-    }
-
-    atak(monster) {
-        monster.hp = 10; 
-        console.log(`${this.nazwa} atakuje ${monster.nazwa} i zadaje 10 obrazen!`);
-    }
-}
-
-class Monster {
+class Character {
     constructor(nazwa, hp) {
         this.nazwa = nazwa;
         this.hp = hp;
     }
 
-    atak(hero) {
-        hero.hp = 5;
-        console.log(`${this.nazwa} atakuje ${hero.nazwa} i zadaje 5 obrazen!`);
+    atak(przeciwnik, obrazenia) {
+        przeciwnik.hp -= obrazenia;
+        przeciwnik.hp = Math.max(0, przeciwnik.hp);
+        console.log(`${this.nazwa} atakuje ${przeciwnik.nazwa} i zadaje ${obrazenia} obrazen!`);
     }
 }
 
+class Hero extends Character {
+    constructor(nazwa, hp, skills) {
+        super(nazwa, hp);
+        this.skills = skills;
+    }
+}
 
+class Monster extends Character {
+    constructor(nazwa, hp) {
+        super(nazwa, hp);
+    }
+}
+
+// Przykladowe uzycie:
 const hero = new Hero("Wojownik", 100, ["Miecz", "Magia"]);
 const monster = new Monster("Smok", 80);
 
-hero.atak(monster);
-monster.atak(hero);
+hero.atak(monster, 10);
+monster.atak(hero, 5);
