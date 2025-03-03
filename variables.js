ddc583eeeb15d1da3141ff59992368597ad3e3f9
Czerwu <mczerw999@gmail.com>
2025-03-03 11:13:32 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+// String -> "Imie"
+// number -> 5
+// boolean -> true / false
+// undefined -> undefined
+// null -> null
+
+const imie = "Michal" 
+const x = 5
+const czyIstnieje = true
+const nieIstnieje = undefined
+const nieMaZawartosci = null
+const arr = [1,"Michal", undefined]
+const obj = {imie, wiek: x, czyNosiOkulary: czyIstnieje} 
